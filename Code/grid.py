3a74ffcd55dd27b9373e2eb1b7d8f860cff50d9f
AUIAG\s108233 <Hariprasath.Muthu@iag.com.au>
2018-03-08 17:10:55 +1100

    Modified grid

--- Code/grid.py
+++ Code/grid.py
@@ -8,8 +8,23 @@ grid = [['.','.','.','.','.','.'],
         ['.','0','0','.','.','.'],
         ['.','.','.','.','.','.']]
 
+#inv_grid = list(9,6)
 print (grid[1][:], len(grid), len(grid[0]))
+r = len(grid)
+c = len(grid[0])
 
-for i in range(0,1):
+inv_grid = [[0] * r] * c
+
+print (inv_grid[1][:], len(inv_grid), len(inv_grid[0]))
+
+
+for i in range(0,r):
    for j in range(0,len(grid[i])):
-       print (grid[j][i])
+       inv_grid[i][j] = grid[j][i]
+
+l = len(inv_grid)
+
+for i in range(0,l):
+   for j in range(0,len(inv_grid[i])):
+        print(inv_grid[i][j],end = ' ')
+   print('')
