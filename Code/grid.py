56d77e1d9a8a165b1cef2951425d69744e333bb8
AUIAG\s108233 <Hariprasath.Muthu@iag.com.au>
2018-03-12 14:17:00 +1100

    grid code is completed

--- Code/grid.py
+++ Code/grid.py
@@ -1,4 +1,4 @@
-import NumPy as np
+import numpy as np
 
 grid = [['.','.','.','.','.','.'],
         ['.','0','0','.','.','.'],
@@ -10,33 +10,21 @@ grid = [['.','.','.','.','.','.'],
         ['.','0','0','.','.','.'],
         ['.','.','.','.','.','.']]
 
-#inv_grid = list(9,6)
-print (grid[1][:], len(grid), len(grid[0]))
 r = len(grid)
 c = len(grid[0])
 
-##inv_grid = [[0] * r] * c
-#inv_grid.clear()
-
-#print (inv_grid[1][:], len(inv_grid), len(inv_grid[0]))
-
 inv_grid = [ [ 0 for i in range(c) ] for j in range(r) ]
 inv_grid.clear()
-for i in range(0,r):
-   for j in range(0,len(grid[i])):
-       #inv_grid[j][i] = grid[i][j]
-      inv_grid.append(grid[i][j])
-   #print(inv_grid[j][i], '  ', grid[i][j],' | ', end = ' ')
- #  print('')
-  # print(inv_grid[:][:])
+for i in range(0,len(grid[0])):
+   for j in range(0,r):
+       inv_grid.append(grid[j][i])
 
-##print(inv_grid[:][:])
 l = len(inv_grid)
-grid_arr = array(inv_grid)
+grid_arr = np.array(inv_grid).reshape(c,r)
 
-#print(inv_grid[3][1])
-##for a in range(0,l):
-##   for b in range(0,len(inv_grid[a])):
-##        print(inv_grid[a][b],end = ' ')
-##   print('')
-print(inv_grid[8])
+for i in range(0,r):
+ print(grid[i][:])
+
+print('Below is the transpose')
+
+print(grid_arr)
