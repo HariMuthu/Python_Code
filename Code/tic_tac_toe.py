3d17c5f4bbde9138ccb874068bd47003476fd3af
AUIAG\s108233 <Hariprasath.Muthu@iag.com.au>
2018-03-12 17:00:45 +1100

    started tic tac toe

---
+++ Code/tic_tac_toe.py
@@ -0,0 +1,12 @@
+import pprint
+print('The Layout','\n')
+print(' TL ','|',' TM ','|',' TR ')
+print('----','|','----','|','----')
+print(' ML ','|',' MM ','|',' MR ')
+print('----','|','----','|','----')
+print(' DL ','|',' DM ','|',' DR ')
+
+tic_tac = {'TL':' ','TM':' ','TR':' '}
+
+print(tic_tac.items())
+pprint.pprint(tic_tac)
